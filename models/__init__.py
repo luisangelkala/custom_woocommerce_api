3ca533df3625b0213b04340ef8abccc3c51757cd
Luis Kala <luisangelkala@gmail.com>
2025-12-30 21:40:13 -0500

    Update product discount

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,7 @@
-from . import order,financing_agency, crm_lead, sale_order_lines, account_move, res_partner
-
+from . import order
+from . import financing_agency
+from . import crm_lead
+from . import sale_order_lines
+from . import account_move
+from . import res_partner
+from . import product
